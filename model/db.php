6a4d0a9a28363eacfb084749d4e2357faef4554f
gcamig <gerard.camig@educem.net>
2024-01-19 10:15:08 +0100

    folder structure and logo

--- model/db.php
+++ model/db.php
@@ -2,7 +2,7 @@
 //TODO #1: Modifica la cadena de connexió amb les teves dades d'accés
 function getConnection()
 {
-    $connString = 'mysql:host=localhost;port=3306;dbname=lovingcats;charset=utf8';
+    $connString = 'mysql:host=localhost;port=3306;dbname=isitec;charset=utf8';
     $user = 'root';
     $pass = '';
     $db = null;
@@ -17,7 +17,7 @@ function getConnection()
 
 // TODO #2: Implementa la funció insertCats($cats) que reb un array de gats i els inserta a la base de dades
 // TODO   : Si un gat ja existeix a la base de dades no s'ha de tornar a inserir (cal comprovar-ho abans d'inserir)
-function insertCats($cats)
+function insertUsers($cats)
 {
     $inserit = false;
     $conn = getConnection();
--- sql/isitec.sql
+++ sql/isitec.sql
@@ -1,18 +1,16 @@
 DROP DATABASE IF EXISTS isitec;
 
-CREATE DATABASE isitec CHARACTER
-SET
-  utf8mb4 COLLATE utf8mb4_0900_as_cs;
+CREATE DATABASE isitec CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE isitec;
 
-create table
-  users (
-    iduser CHAR(8) PRIMARY KEY AUTO_INCREMENT,
-    mail CHAR(40) UNIQUE,
+CREATE TABLE users (
+    iduser INT AUTO_INCREMENT PRIMARY KEY,
+    mail VARCHAR(40) UNIQUE,
     passHash VARCHAR(60),
     userFirstName VARCHAR(60),
     userLastName VARCHAR(120),
     creationDate DATETIME,
-    removeDare DATETIME,
+    removeDate DATETIME,
     lastSignIn DATETIME,
-    active TINYINT (1)
-  );
+    active TINYINT(1)
+);
