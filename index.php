1dd2a00f8779eec7788b339cfc3353aa938b3e4e
gcamig <gerard.camig@educem.net>
2024-01-19 10:48:06 +0100

    si

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <title>#</title>
-    <meta charset="utf-8">
-    <meta name="author" content="author">
-    <meta name="keywords" content="keywords">
-    <meta name="description" content="description">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/main.css">
-  </head>
-  <body>
-  </body>
+
+<head>
+  <title>Isitec: Login</title>
+  <meta charset="utf-8">
+  <meta name="author" content="author">
+  <meta name="keywords" content="keywords">
+  <meta name="description" content="description">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="./img/logo.png">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+    <input name="user" type="email" placeholder="Introdueix el teu email" value="<?= isset($_POST["user"]) ? htmlspecialchars($_POST["user"]) : "" ?>" autofocus required>
+    <input name="pass" type="password" placeholder="Introdueix el teu password" required>
+    <input type="submit" value="Login">
+  </form>
+</body>
+
 </html>
