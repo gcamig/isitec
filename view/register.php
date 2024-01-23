1fb7bef8afd199368168f370e3cf90beefef066c
gcamig <gerard.camig@educem.net>
2024-01-23 10:27:53 +0100

    Separated login and register forms and imporved css

---
+++ view/register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <title>#</title>
+  <meta charset="utf-8">
+  <meta name="author" content="author">
+  <meta name="description" content="description">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/register.css">
+</head>
+
+<body>
+  <section class="text-animation-box">
+    <div>
+      <h1 class="typeHeader">Welcome back!</h1>
+      <p class="typeText"></p>
+    </div>
+  </section>
+  <section class="form-box">
+    <h1>Sign Up</h1>
+    <form class="sign-up-form">
+      <div class="sign-up-grid">
+        <div class="input-box">
+          <label for="usr"><ion-icon name="person-outline"></ion-icon></label>
+          <input type="text" id="usr" name="username" required placeholder="">
+          <span>Username</span>
+        </div>
+        <div class="input-box">
+          <label for="email"><ion-icon name="mail-outline"></ion-icon></label>
+          <input type="email" id="email" name="email" required placeholder="">
+          <span>Email</span>
+        </div>
+        <div class="input-box">
+          <input type="text" id="firstname" name="firstname" required placeholder="">
+          <span>First Name</span>
+        </div>
+        <div class="input-box">
+          <input type="text" id="lastname" name="lastname" required placeholder="">
+          <span>Last Name</span>
+        </div>
+        <div class="input-box">
+          <label for="pswd"><ion-icon name="lock-closed-outline"></ion-icon></label>
+          <input type="password" id="pswd" name="password" required placeholder="">
+          <span>Password</span>
+        </div>
+        <div class="input-box">
+          <label for="veri-pswd"><ion-icon name="lock-closed-outline"></ion-icon></label>
+          <input type="password" id="veri-pswd" name="veri-pswd" required placeholder="">
+          <span>Verify Password</span>
+        </div>
+      </div>
+      <button class="button-86">Sign Up</button>
+    </form>
+    <div class="change-form">
+      <p>Already have an account?</p>
+      <a href="/index.php">Sign In</a>
+    </div>
+  </section>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="/js/typing.js"></script>
+</body>
+
+</html>
