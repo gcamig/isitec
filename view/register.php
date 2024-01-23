ee45f960c2caf413776da1831e73457371944445
viapl <viaplanaferran@gmail.com>
2024-01-23 16:15:17 +0100

    register js fixed

--- view/register.php
+++ view/register.php
@@ -88,7 +88,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   </section>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-  <script src="/js/typing.js"></script>
+  <script src="../js/typing.js"></script>
 </body>
 
 </html>
