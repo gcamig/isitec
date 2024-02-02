ee4488a28f58e8560e3ddaec1750efd4fbfad122
viapl <viaplanaferran@gmail.com>
2024-02-02 11:51:23 +0100

    require

--- controller/controller.php
+++ controller/controller.php
@@ -1,4 +1,5 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
 require_once "../model/db.php";
 
 function loginUser($userOrEmail, $pass){
@@ -9,3 +10,34 @@ function insertUser($user){
     return insertUserDB($user);
 }
 
+function verificationEmail($user){
+    $mail = new PHPMailer();
+    $mail -> IsSMTP();
+    //Configuració del servidor de Correu
+    //Modificar a 0 per eliminar msg error
+    $mail->SMTPDebug = 2;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    //Credencials del compte GMAIL
+    $mail->Username = '';
+    $mail->Password = '';
+
+    //Dades del correu electrònic
+    $mail->SetFrom('user@gmail.com','Test');
+    $mail->Subject = 'Correu de test';
+    $mail->MsgHTML('Prova');
+    
+    //Destinatari
+    $address = 'destinatari@servidor.com';
+    $mail->AddAddress($address, 'Test');
+
+    //Enviament
+    $result = $mail->Send();
+    if(!$result){
+        echo 'Error: ' . $mail->ErrorInfo;
+    }else{
+        echo "Correu enviat";
+    }
+}
