740bac2651a0db2c167cc6ea0f56bac3c8f2a02f
Gerard Camí <dev.cxmi@gmail.com>
2024-03-15 10:11:21 +0100

    Add margin to toggle area text and adjust rotation speed

--- js/background.js
+++ js/background.js
@@ -53,7 +53,7 @@ function generateShape() {
 
 function moveShape(shape) {
   const speed = 1 + Math.random() * 2;
-  const rotationSpeed = 0.5 + Math.random() * 2;
+  const rotationSpeed = (0.5 + Math.random() * 2) / window.screen.refreshRate;
   const angle = (Math.random() - 0.5) * 2 * Math.PI;
   let rotation = 0;
   const dx = Math.cos(angle) * speed;
@@ -88,6 +88,8 @@ function moveShape(shape) {
 
   move();
 }
+
+
 
 function generateShapes(amountShapes) {
   for (let i = 0; i < amountShapes; i++) {
