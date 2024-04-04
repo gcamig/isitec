fa600cd085e2cb21002feeb1faff2319732699ee
Gerard Camí <dev.cxmi@gmail.com>
2024-04-05 00:56:09 +0200

    Refactor CSS and PHP code for course detail page

--- view/course_detail.php
+++ view/course_detail.php
@@ -113,12 +113,12 @@ if (!isset($_COOKIE['PHPSESSID'])) {
       </section>
       <!-- subir archivos + boton  -->
       <?php if (isFounder($_SESSION['username'], $course['title'])): ?>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+        <form class="flex flex-col gap-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
           <div class="file-upload-panel" onclick="document.getElementById('fileInput').click();">
             Subir nuevos videos
             <input type="file" name="video" id="fileInput" class="file-upload-input">
           </div>
-          <input type="submit" name="submit" value="Añadir">
+          <input id="btn-add" type="submit" name="submit" value="Añadir">
           <?php echo '<input type="hidden" name="courseID" value ="' . $course['title'] . '">'; ?>
         <?php endif; ?>
         <section class="course-content">
